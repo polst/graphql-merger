dfe2894a8036ce88fcbc15543a8ec9fdf160aa36
Paul <devel.pa@gmail.com>
2016-12-26 19:48:24 +0100

    Holy code.

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # graphql-merger
 Merge definitions of GraphQL scheme found in given folder
+
+NOTES: 
+* Written with ES6+. Tested with Babel and Node 6.9.2 
+* Not published to npm repo.
+* MIT
+* Suggestions are welcome
+* Inspiration taken from many places
+
+##Usage
+
+####`import graphqlModulesMerger from '../utils/graphqlMerger';`
+
+Merges graphql modules without adding 
+```graphql schema
+    schema {
+      query: Query
+      mutation: Mutation
+    }
+```
+Good for creating GraphQL packages to be used later.
+
+####`import {graphqlMerger} from '../utils/graphqlMerger';`
+
+Merges graphql modules adding at the end 
+```graphql schema
+    schema {
+      query: Query
+      mutation: Mutation
+    }
+```
+Good for delivery of the schema.
+
+###Returns
+
+`{Schema, Subcriptions}`
+
+##Schema defs
+
+Schema must be defined this way: 
+!!!ATTN!!!: notice `extend` keyword on `Query` and `Mutation`
+```ecmascript 6
+// language="GraphQL Schema"
+const typeDefs = `
+  # the Todo type with an id and a text 
+  type Todo {
+    id: Int                     
+    text: String
+  }
+  extend type Query {
+    # Find a Todo with id
+    todo(id: Int): Todo         
+    # Find All todos
+    todos: [Todo]               
+  }
+  extend type Mutation {
+    # Add a new Todo with a content text
+    addTodo(text: String): Boolean
+    # Delete a todo with id
+    deleteTodo(id: Int): Boolean
+  } 
+`;
+
+const resolvers = (() => {
+  let todos = [];
+
+  const genId = (_ => {
+    let id = 0;
+    return _ => ++id;
+  })();
+
+  return {
+    Query: {
+      todo:     (_, { id }) => todos.find(todo => todo.id === id),
+      todos:              _ => todos
+    },
+    Mutation: {
+      addTodo:  (_, {text}) => (todos.push({id: genId(), text}), true),
+      deleteTodo: (_, {id}) => (todos = todos.filter(todo => todo.id !== id), true)
+    }
+  }
+})();
+
+export { typeDefs, resolvers }
+```
+
+NOTE: also, you can include in the returned object the subscriptions 
---
+++ index.js
@@ -0,0 +1,82 @@
+//https://gist.github.com/icebob/553c1f9f1a9478d828bcb7a08d06790a
+import path from 'path';
+import merge from 'lodash.merge';
+import getGlobbedFiles from './lib/getGlobbedFiles';
+import x from './lib/mandatory';
+
+// --- MERGE RESOLVERS
+const mergeModuleResolvers = function mergeModuleResolvers(moduleResolvers, baseResolvers) {
+  moduleResolvers.forEach((module) => {
+    baseResolvers = merge(baseResolvers, module);
+  });
+  return baseResolvers;
+};
+
+const mergeData = function mergeData(dest, src) {
+  if (src) {
+    if (Array.isArray(src)) {
+      dest.concat(src);
+    }
+    else {
+      dest.push(src);
+    }
+  }
+};
+
+const graphqlMerger = function graphqlMerger(dir = x`dir`) {
+  let {Schema, Subscriptions} = graphqlModulesMerger(dir);
+
+  const FinalSchema = {
+    typeDefs: [`
+    ${Schema.typeDefs}
+    
+    schema {
+      query: Query
+      mutation: Mutation
+    }
+  `
+    ],
+    resolvers: Schema.resolvers
+  };
+
+  return {Schema: FinalSchema, Subscriptions};
+};
+
+export {graphqlMerger};
+
+export default function graphqlModulesMerger(dir = x`dir`) {
+
+  let moduleTypeDefinitions = [];
+  let moduleResolvers = [];
+  let moduleSubscriptions = [];
+
+  let files = getGlobbedFiles(path.join(dir, "**", "*.graphql.js"));
+
+  // Load schema files
+  files.forEach((file) => {
+    let moduleSchema = require(path.resolve(file));
+    mergeData(moduleTypeDefinitions, moduleSchema.typeDefs);
+    mergeData(moduleResolvers, moduleSchema.resolvers);
+    mergeData(moduleSubscriptions, moduleSchema.subscriptions);
+  });
+
+  // --- MERGE TYPE DEFINITONS
+  const schema = `
+    type Query {
+      # Extended by typeDefs
+       bogusBulderTricksTheQueryCompiler: Int
+    }
+    type Mutation {
+      # Extended by typeDefs
+      bogusBulderTricksTheMutationCompiler: Int
+    }
+    ${moduleTypeDefinitions.join("\n")}
+  `;
+
+  const Schema = {
+    typeDefs: schema,
+    resolvers: mergeModuleResolvers(moduleResolvers, {})
+  };
+
+  return {Schema, Subscriptions: moduleSubscriptions};
+};
